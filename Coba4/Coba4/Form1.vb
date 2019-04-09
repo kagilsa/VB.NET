4efcbae6b70ae9880085656de940f808489ef00c
kagilsa <47039600+kagilsa@users.noreply.github.com>
2019-04-09 18:58:59 +0700

    Add files via upload

---
+++ Coba4/Coba4/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Text1.Text = "" And Text2.Text = "" Then
+            MsgBox("Coba Lagi")
+        Else
+            MsgBox("Login Berhasil")
+            Form2.Show()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If MsgBox("Yakin akan Keluar?", MsgBoxStyle.Question Or MsgBoxStyle.YesNo) <> Windows.Forms.DialogResult.Yes Then
+        Else
+            Me.Close()
+        End If
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Text1.Text = ""
+        Text2.Text = ""
+    End Sub
+End Class
