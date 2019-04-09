9853a3c587a07877711f15665ec2d0b2326580dd
kagilsa <47039600+kagilsa@users.noreply.github.com>
2019-04-09 19:00:36 +0700

    Add files via upload

---
+++ TutorialJamDigital/TutorialJamDigital/JamDigital.Designer.vb
@@ -0,0 +1,59 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class JamDigital
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Jam = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Jam
+        '
+        Me.Jam.AutoSize = True
+        Me.Jam.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Jam.Location = New System.Drawing.Point(331, 52)
+        Me.Jam.Name = "Jam"
+        Me.Jam.Size = New System.Drawing.Size(83, 25)
+        Me.Jam.TabIndex = 0
+        Me.Jam.Text = "Label1"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'JamDigital
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Jam)
+        Me.Name = "JamDigital"
+        Me.Text = "JamDigital"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Jam As Label
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ TutorialJamDigital/TutorialJamDigital/JamDigital.vb
@@ -0,0 +1,5 @@
+﻿Public Class JamDigital
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Jam.Text = TimeOfDay
+    End Sub
+End Class
