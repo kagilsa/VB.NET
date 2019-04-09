6b35a94a3170a2a298fab24bc03b629dd0aec227
kagilsa <47039600+kagilsa@users.noreply.github.com>
2019-04-09 18:57:59 +0700

    Coba3

---
+++ Coba3/Coba3/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Text1.Text = “” Then
+            MsgBox(“Isi dulu Nama anda”)
+        Else
+            MsgBox(“Nama Saya ” + Text1.Text + “ dan saya sedang belajar Visual Studio”)
+        End If
+    End Sub
+End Class
