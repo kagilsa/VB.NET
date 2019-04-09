dc778ed4d1c3ef13c811e964a0cce85b417cab47
kagilsa <47039600+kagilsa@users.noreply.github.com>
2019-04-09 18:59:29 +0700

    Add files via upload

---
+++ Coba2/Coba2/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Coba2.Models;
+
+namespace Coba2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Person> _person = new List<Person>()
+            {
+                new Person {Name = "Raka Gilang Prakarsa", Country = "Indonesia" }
+            };
+            return View();
+
+        }
+    }
+
+}
---
+++ Coba2/Coba2/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coba2.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+}
